274540ef82a888b8ad895d9f923333b0556762e1
xaexoon <xaexoon07@gmail.com>
2024-02-12 22:41:07 +0900

    discussnet default setting

---
+++ discussnet/src/main/java/teamsm/project/discussnet/DiscussnetApplication.java
@@ -0,0 +1,13 @@
+package teamsm.project.discussnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussnetApplication.class, args);
+	}
+
+}
---
+++ discussnet/src/test/java/teamsm/project/discussnet/DiscussnetApplicationTests.java
@@ -0,0 +1,13 @@
+package teamsm.project.discussnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
